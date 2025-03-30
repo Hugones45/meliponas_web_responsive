b25de8f134058dacd743fb5b129fadb7cd03fc8f
Hugo <hugones45@gmail.com>
2025-03-30 10:13:56 -0300

    put relative position on apiculture side

--- src/Pages/PageCuriosities/index.js
+++ src/Pages/PageCuriosities/index.js
@@ -28,7 +28,7 @@ export const PageCuriosities = () => {
                         <div className={styles.description_container} >
                             <h1>Meliponiculture</h1>
                             <p style={{ zIndex: '10' }}>Melipoculture is the term used for the activity of raising stingless bees, commonly known as meliponas. Unlike beekeeping, the management of meliponas rarely requires protection, except for some species. Although meliponiculture has grown in recent years, many Brazilians are still unaware of native bees, and this is due to the historical lack of incentive and interest from the Brazilian government in ecology and wildlife preservation.</p>
-                            <p><span style={{ zIndex: '10', position: 'relative' }} />Brazilian soil is home to more than 250 species of stingless bees, which play a crucial role in balancing the biomes.<span style={{ zIndex: '10', position: 'relative' }} >. Cultivating these bees not only generates income but also provides knowledge about the land and benefits the entire ecosystem's health</span></p>
+                            <p><span style={{ zIndex: '10', position: 'relative' }} /><span style={{ zIndex: '10', position: 'relative' }} >Brazilian soil is home to more than 250 species of stingless bees, which play a crucial role in balancing the biomes.. Cultivating these bees not only generates income but also provides knowledge about the land and benefits the entire ecosystem's health</span></p>
                         </div>
 
 
@@ -46,9 +46,9 @@ export const PageCuriosities = () => {
 
                         <div className={styles.description_container}>
                             <h1>Apiculture</h1>
-                            <p>Apiculture is the practice of raising honeybees of the Apis mellifera genus, which are exotic bees originating from Africa and Europe. They arrived in Brazil alongside the colonizers, and initially, their cultivation focused on wax production, which was widely used for religious activities. In modern times, apiculture has become a significant source of income and plays a crucial role in Brazil's economy.</p>
+                            <p><span style={{ zIndex: '10', position: 'relative' }} >Apiculture is the practice of raising honeybees of the Apis mellifera genus, which are exotic bees originating from Africa and Europe. They arrived in Brazil alongside the colonizers, and initially, their cultivation focused on wax production, which was widely used for religious activities. In modern times, apiculture has become a significant source of income and plays a crucial role in Brazil's economy.</span></p>
 
-                            <p>However, excessive management and disregard for ecological consequences have been threatening the balance of native fauna. It is important to remember that all bee species need to be preserved, and it is up to us, as caretakers, to find a safe path for their preservation and prosperity.</p>
+                            <p><span style={{ zIndex: '10', position: 'relative' }} >However, excessive management and disregard for ecological consequences have been threatening the balance of native fauna. It is important to remember that all bee species need to be preserved, and it is up to us, as caretakers, to find a safe path for their preservation and prosperity.</span></p>
                         </div>
 
                     </div>
